24f7eba101b3195c69a4aef8c5d193ac824ef457
SYH <syh080315@outlook.com>
2026-01-26 02:01:11 +0800

    add small enhancements

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: './', // 关键：使用相对路径确保在任何目录下都能运行
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [vue()],
+    // 判断逻辑：
+    // 如果是开发模式 (serve)，路径用 '/'
+    // 如果是打包模式 (build)，路径用 './'
+    base: command === 'serve' ? '/' : './',
+  }
 })

125214e31486c4b81d1af873bf935801df33bef9
SYH <syh080315@outlook.com>
2026-01-26 01:45:05 +0800

    add small enhancements

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/syh/', // 关键：使用相对路径确保在任何目录下都能运行
+  base: './', // 关键：使用相对路径确保在任何目录下都能运行
 })
